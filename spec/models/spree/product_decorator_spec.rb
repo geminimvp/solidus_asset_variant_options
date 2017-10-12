dd6fe26c1c3bc4a1710936c1fad4b7afef21a2ed
fastjames <fastjames@gmail.com>
2017-10-12 14:29:55 -0500

    Include master variant images
    When determining the images to show for a selected variant, we need to
    include all images associated with the master variant as well.

--- spec/models/spree/product_decorator_spec.rb
+++ spec/models/spree/product_decorator_spec.rb
@@ -3,19 +3,20 @@ require 'spec_helper'
 describe Spree::Product do
   let(:product) { create :product }
   let(:variant) { create :base_variant, product: product }
-  let(:image_green) { create :image, viewable_type: 'Spree::Variant' }
-  let(:image_blue) { create :image, viewable_type: 'Spree::Variant' }
+  let(:image_green) { create :image }
+  let(:image_blue) { create :image }
 
   before do
-    variant.images << image_green
-    variant.images << image_blue
-    variant.save
+    Spree::VariantImage.create!(variant: variant, image: image_green)
+    Spree::VariantImage.create!(variant: variant, image: image_blue)
+    variant.reload
+    product.reload
   end
 
   describe '#variant_images' do
     it 'returns unique list of variant images' do
-      expect(product.reload.variant_images.size).to eq(2)
-      expect(product.reload.variant_images).to include(image_blue, image_green)
+      expect(product.variant_images.size).to eq(2)
+      expect(product.variant_images).to include(image_blue, image_green)
     end
   end
 
